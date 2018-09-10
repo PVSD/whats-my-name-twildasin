926dcda4105d431b830ba162a87d435c72fa2e43
twildasin <tjwild127@aol.com>
2018-09-10 11:46:25 -0400

    completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        String first;
+        String last;
+
+        Scanner kbin = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        first = kbin.nextLine();
+        System.out.println("What is your last name?");
+        last = kbin.nextLine();
+        System.out.println("Your full name is " + first + " " + last);
+
+
+
+
+
     }
 }
